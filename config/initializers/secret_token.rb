a19cbb4bdb72594822a06c3189aafa48c488dd4e
Nicholas Audo <naudo@elctech.com>
2012-08-07 21:22:47 -0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThisIsWhyYourAppIsSlow::Application.config.secret_token = 'd3d5c850b5d5fbf23d65b8952a57a18e9a1cdd3f749fe5cea920b2d2448904e98f475cec7953e4af5c1c0e658bcc4f7f8430588f6a3ae39082c870b31e32fda6'
